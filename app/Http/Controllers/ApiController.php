7aab2b3fbd0ea718d60ea09f36147e29daa54825
shiro <dbeltran486@gmail.com>
2023-11-06 16:23:14 -0400

    creacio de modelos y programacion

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Datos;
+
+class ApiController extends Controller
+{
+    public function getData(Request $request){
+        $data = Datos::where("angulo", "=", $request->angulo)->get();
+        $mydata = [];
+        if(count($data) > 0){
+            $value = rand(0, count($data) - 1);
+            $mydata = $data[$value];
+        }
+        return response()->json($mydata);
+    }
+}
